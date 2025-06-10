5731e76cdf64fece96bebe7416a5604e325a1ac5
tsukan <124868947+ikumi7184@users.noreply.github.com>
2025-06-11 07:05:16 +0900

    コメント部分を修正

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import { Button } from '@mui/material'
+import React from "react";
+import { Button } from "@mui/material";
 import firebase from "firebase/compat/app";
 import { auth } from "../firebase.js";
 
 const SignIn = () => {
-    const signInWithGoogle = () => {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        auth.signInWithPopup(provider);
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return (
+    <div>
+      <Button onClick={signInWithGoogle}>Googleでログインする</Button>
+    </div>
+  );
+};
 
-    }
-    return (
-        <div>
-            <Button onClick={signInWithGoogle}>Googleでログインする</Button>
-        </div>
-    )
-}
-
-export default SignIn
+export default SignIn;
