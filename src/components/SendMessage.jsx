c6af8f8175e8faf96e65b3c7b8444f7b24c78d4b
tsukan <124868947+ikumi7184@users.noreply.github.com>
2025-06-12 06:35:37 +0900

    追加機能：google認証、空文字送信不可、iconクリックで送信

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,44 +1,57 @@
-import React, { useState } from 'react'
-import { auth, db } from '../firebase';
-import firebase from 'firebase/compat/app';
-import { Input } from '@mui/material';
+import React, { useState } from "react";
+import { auth, db } from "../firebase";
+import firebase from "firebase/compat/app";
+import { Input } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
 const SendMessage = () => {
-    const [message, setMessage] = useState("");
-    function sendMessage(e) {
-        e.preventDefault();
+  const [message, setMessage] = useState("");
+  function sendMessage(e) {
+    e.preventDefault();
 
-        const { uid, photoURL } = auth.currentUser;
-
-        db.collection("messages").add({
-            text: message,
-            photoURL,
-            uid,
-            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        });
-        setMessage("");
+    if (message.trim() === "") {
+      return; // ここで処理を止める！
     }
-    return (
-        <div>
-            <form onSubmit={sendMessage}>
-                <div className="sendMsg">
-                    <Input style={{
-                        width: "78%",
-                        fontSize: "15px",
-                        fontWeight: "550",
-                        marginLeft: "5px",
-                        marginBottom: "-3px",
-                    }}
-                        placeholder='メッセージを入力' type="text" onChange={(e) => setMessage(e.target.value)} value={message} />
-                    <SendIcon style={{
-                        color: "#7ac2ff",
-                        marginLeft: "20px"
-                    }} />
-                </div>
-            </form>
-        </div>
-    )
-}
 
-export default SendMessage
+    const { uid, photoURL } = auth.currentUser;
+
+    db.collection("messages").add({
+      text: message,
+      photoURL,
+      uid,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setMessage("");
+  }
+  return (
+    <div>
+      <form onSubmit={sendMessage}>
+        <div className="sendMsg">
+          <Input
+            style={{
+              width: "78%",
+              fontSize: "15px",
+              fontWeight: "550",
+              marginLeft: "5px",
+              marginBottom: "-3px",
+            }}
+            placeholder="メッセージを入力"
+            type="text"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+          <SendIcon
+            className="send_icon"
+            onClick={sendMessage}
+            style={{
+              color: "#7ac2ff",
+              marginLeft: "20px",
+            }}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SendMessage;
